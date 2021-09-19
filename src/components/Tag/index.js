8d07f4a078bc1bb0131b00b95d0be32852de9d22
Rufai Mustapha <etop94@gmail.com>
2021-09-19 21:36:08 +0100

    class used in Tag Component

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -2,12 +2,12 @@ import "./index.css"
 function Tag(){
     return (
         <section id="tag">
-           <button type="button" class="btn btn-outline-info" style={{"backgroundColor": "black","color": "white"}}>All</button>
-            <button type="button" class="btn btn-outline-warning">Fathers</button>
-            <button type="button" class="btn btn-outline-warning">Boys</button>
+           <button type="button" className="btn btn-outline-info" style={{"backgroundColor": "black","color": "white"}}>All</button>
+            <button type="button" className="btn btn-outline-warning">Fathers</button>
+            <button type="button" className="btn btn-outline-warning">Boys</button>
            
-            <button type="button" class="btn btn-outline-warning">Related items</button>
-            <button type="button" class="btn btn-outline-warning">Recent</button>
+            <button type="button" className="btn btn-outline-warning">Related items</button>
+            <button type="button" className="btn btn-outline-warning">Recent</button>
         </section>
     )
 }
