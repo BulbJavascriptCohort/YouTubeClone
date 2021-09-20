24211d1490f0d156507e23ab52bc4f86fa796c33
Abiodun Segun <84022331+realshegz-github@users.noreply.github.com>
2021-09-20 16:14:30 +0100

    add video to the homepage

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,8 +1,14 @@
-import video from "./video.mp4";
+import ReactPlayer from 'react-player/youtube';
 function VideoPlayer() {
 	return (
 		<section id="VideoPlayer">
-			<video src={video}></video>
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
